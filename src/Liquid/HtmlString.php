81cc4d2c6e49a0c0cd9c8a1f711b15793fd6f3df
Georgi Nachev <jooorooo@gmail.com>
2022-10-18 17:54:38 +0300

    Add files via upload

---
+++ src/Liquid/HtmlString.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Liquid;
+
+class HtmlString
+{
+    /**
+     * The HTML string.
+     *
+     * @var string
+     */
+    protected $html;
+
+    /**
+     * Create a new HTML string instance.
+     *
+     * @param  string  $html
+     * @return void
+     */
+    public function __construct($html)
+    {
+        $this->html = $html;
+    }
+
+    /**
+     * Get the HTML string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->html;
+    }
+}
